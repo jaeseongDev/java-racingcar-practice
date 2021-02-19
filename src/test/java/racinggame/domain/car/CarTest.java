acc9f2cc819dec9273aad345ace3025cdae3e509
jaeseongDev <qkrwotjd1445@naver.com>
2021-02-19 19:57:22 +0900

    refactor : 자동차 이름 유효성 검사에 대한 로직에 대한 테스트 코드를 NameTest로 옮김

---
+++ src/test/java/racinggame/domain/car/CarTest.java
@@ -0,0 +1,5 @@
+package racinggame.domain.car;
+
+public class CarTest {
+
+}
--- src/test/java/racinggame/domain/car/NameTest.java
+++ src/test/java/racinggame/domain/car/NameTest.java
@@ -1,12 +1,12 @@
-package racinggame.domain;
+package racinggame.domain.car;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racinggame.domain.car.Car;
 
-public class CarTest {
+public class NameTest {
+
     @ParameterizedTest(name = "자동차 이름이 1~5자가 아닐 경우 예외 발생")
     @ValueSource(strings = {"", "abcdef", "abcdefg"})
     public void init_validateCarNameLength_ExceptionThrown(String name) {
